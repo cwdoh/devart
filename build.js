9df7e240979c782d031b6f616b4e3916aa440f24
Chang W. Doh <changwook.doh@gmail.com>
2014-02-24 16:25:18 +0900

    Added static page & its generator for no script option

---
+++ build.js
@@ -0,0 +1,28 @@
+var fs = require('fs'),
+    path = require("path"),
+    marked = require('marked');
+
+var basePath = process.cwd(),
+    headerPath = path.join(basePath, 'template/header.html'),
+    markdownPath = path.join(basePath, 'README.md'),
+    footerPath = path.join(basePath, 'template/footer.html'),
+    outputPath = path.join(basePath, 'noscript.html');
+
+// remove output file.
+fs.exists(outputPath, function (exists) {
+  if ( exists ) {
+    console.log( "REMOVE: " + outputPath );
+    fs.unlinkSync(outputPath);
+  }
+
+  // header
+  fs.appendFileSync( outputPath, fs.readFileSync( headerPath ) );
+
+  // markdown body
+  fs.appendFileSync( outputPath, marked( fs.readFileSync( markdownPath ).toString() ) );
+
+  // footer
+  fs.appendFileSync( outputPath, fs.readFileSync( footerPath ) );
+
+  console.log( "GENERATED: " + outputPath );
+});
